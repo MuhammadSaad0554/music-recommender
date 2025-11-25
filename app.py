9c5a75de5b919058a14d51cbf7d70d2be2d3b3e7
Muhammad Saad <ms4689@georgetown.edu>
2025-11-24 21:18:05 -0500

    Update app.py message update

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-
 import streamlit as st
 
 st.set_page_config(
-    page_title="Music Recommender",
-    page_icon="🎵",
+    page_title="Introduction",
 )
 
-st.title("Music Recommendation System Using Spotify Data")
-st.write("Use sidebar to navigate between pages")
+st.title("Introduction")
+
+st.write("""
+This application demonstrates a simple content-based music recommendation system built 
+using Spotify audio features and unsupervised learning techniques. The pipeline preprocess the data, 
+scales numerical audio features, reduces dimensions using PCA, 
+clusters songs based on K-means, and finall generates recommendations
+using cosine similarity within largely similar genres.
+
+Before you jump into the recommender, **please carefully read the User Instructions** from the sidebar.
+""")
